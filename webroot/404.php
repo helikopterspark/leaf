e26955a3dc435552943c7b37f849edcbc4b35280
Carl Ramsell <helikopterspark@bredband.net>
2015-02-01 23:32:28 +0100

    404 upd

--- webroot/404.php
+++ webroot/404.php
@@ -6,8 +6,6 @@
 // Include the essential config-file which also creates the $leaf variable with its defaults.
 include(__DIR__.'/config.php'); 
 
-
-
 // Do it and store it all in variables in the Leaf container.
 $leaf['title'] = "404";
 $leaf['header'] = "";
@@ -16,7 +14,6 @@ $leaf['footer'] = "";
 
 // Send the 404 header 
 header("HTTP/1.0 404 Not Found");
-
 
 // Finally, leave it all to the rendering phase of Leaf.
 include(LEAF_THEME_PATH);
