ce0d9d583c98b30b93551c5c576ce4fc43ca9a2f
Carl Ramsell <carlramsell@Carls-Mac-mini.local>
2015-03-03 23:33:54 +0100

    kmom02: update report

--- webroot/reports/Kmom02.php
+++ webroot/reports/Kmom02.php
@@ -12,6 +12,7 @@ $html = <<<EOD
         <p>En kalenderklass, CCalendar, bygger upp en sida genom att använda sig av en månadsklass, CCalendarMonth. En instans av denna klass skapas genom att skicka in månad och år. Utifrån dessa värden kollar den upp veckonummer i månaden och skapar en array med veckonumren. Med hjälp av denna array skriver den ut ingående dagar för respektive vecka. Den visar även veckonummer, söndagar, markerar dagens datum och gör datum utanför aktuell månad gråa och lite mindre. Varje månad visar en egen bild. Sidorna validerar korrekt i Unicorn vad jag kan se, vilket även gäller tärningsspelet.</p>
         <p>Jag hade gärna fortsatt och gjort aside-kalendern, namnsdagar och fler helgdagar men tiden rann ut för att hinna lämna in i tid. I övrigt gjorde jag om redovisningssidan lite så att den laddar in separata filer för varje kursmoment och bygger även en länklista. Jag gjorde detta för att få lite mer överskådliga filer för redovisningssrapporterna.</p>
         <p>Det blev lite mastigt att göra bägge uppgifterna och en reflektion är att kalenderuppgiften nästan hade kunnat få vara ett eget kursmoment och då i samband med en databas för namnsdagar och helgdagar.</p>
+        <p>Tillägg: Jag korrigerade problemet med att det gick att ladda om sidan för att fuska.</p>
  	</div>
  	<div style="text-align:center;">
  	<p><a href="#">Upp</a></p>
