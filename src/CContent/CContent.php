a05cfb84f95ea04beba4df53f8caa4b9ce06c42a
Carl Ramsell <carlramsell@Carls-Mac-mini.local>
2015-04-12 21:52:23 +0200

    kmom05: update hostserver

--- src/CContent/CContent.php
+++ src/CContent/CContent.php
@@ -579,7 +579,7 @@ EOD;
             // Restore the database to its original settings BTH server
             $sql = 'reset.sql';
             $mysql = '/usr/bin/mysql';
-            $host = 'localhost';
+            $host = 'blu-ray.student.bth.se';
             $login = DB_USER;
             $password = DB_PASSWORD;
             $reset = null;
@@ -595,10 +595,10 @@ EOD;
              */
 
             if (isset($_POST['restore']) || isset($_GET['restore'])) {
-                $cmd = "$mysql -h{$host} -u{$login} -p'{$password}' < $sql 2>&1";
+                $cmd = "$mysql -h{$host} -u{$login} -p{$password} < $sql 2>&1";
                 $res = exec($cmd);
                 //$reset = "<p>Databasen är återställd via kommandot<br/><code>{$cmd}</code></p><p>{$res}</p>";
-                $reset = "<p>Databasen är återställd.</p>";
+                $reset = "<p>Databasen är återställd.</p><p>{$res}</p>";
             }
             $output = <<<EOD
     		<form method=post>
